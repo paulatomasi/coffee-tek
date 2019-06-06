3a41197feceded7b039d552700770cb308bbad7f
Paula Tomasi <paula.andrade@cwi.com.br>
2019-06-06 20:25:39 -0300

    on boarding screen

---
+++ coffee-tek/Designable.swift
@@ -0,0 +1,16 @@
+//
+//  Designable.swift
+//  coffee-tek
+//
+//  Created by Paula Tomasi Andrade on 06/06/19.
+//  Copyright © 2019 CWI Software. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class DesignableView: UIView {}
+
+
+@IBDesignable
+class DesignableButton: UIButton {}
---
+++ coffee-tek/OnBoardingController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  coffee-tek
+//
+//  Created by Paula Tomasi Andrade on 06/06/19.
+//  Copyright © 2019 CWI Software. All rights reserved.
+//
+
+import UIKit
+
+class OnBoardingController: UIViewController {
+
+}
+
